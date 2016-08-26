2196ac0d565417b30b0e4ed954dccf77e50263a6
jjarmain <jjarmain@gmail.com>
2016-08-26 15:23:33 -0700

    working on testing

--- test/reducers/reducer_spec.js
+++ test/reducers/reducer_spec.js
@@ -1,8 +1,30 @@
-// import {List, Map, fromJS} from 'immutable';
-// import {expect} from 'chai';
+import {List, Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+import reducer from '../../src/reducers';
 
-// import reducer from '../../src/reducers';
+describe('reducer spec', () =>{
+    beforeEach('setup state', () => {
+        var initialState = fromJS({add:'',
+            ingredients:[],
+            results:[],
+            page:0,
+            recipe:0,
+            error:''
+        });
+    });
+    it('',()=>{
+        // set action
+        const action = {type:'ADD_INGREDIENT' ,text: 'h'};
+        // get state
+        var nextState = reducer(initialState, action);
+        // assert state
+        expect(nextState).to.equal(fromJS({add:'h',
+            ingredients:[],
+            results:[],
+            page:0,
+            recipe:0,
+            error:''
+        }));
+    })
 
-// describe('reducer spec', () =>{
-
-// });
+});
