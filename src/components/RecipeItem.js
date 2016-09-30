dc355051afb2789b555aa64bbcb24d5d63bac2ce
jjarmain <jjarmain@gmail.com>
2016-09-30 09:28:46 -0700

    working on stuff

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import {ListItem} from 'material-ui/List';
 
+const haveIngredient = (ingredient) => {
+    return <span style="font-weight: bold;">{ingredient}</span>
+}
+const Ingredient = (ingredient) => {
+    return <span>{ingredient}</span>    
+}
+
 export default class RecipeItem extends Component {
+    listIngredients(ingredients, ingredientsList) {
+        return ingredients.map(x => {
+            if (ingredientsList.indexOf(x) > -1) {
+                return haveIngredient(x);
+            } else {
+                return Ingredient(x);
+            }
+        });
+    }
     render() {
         var jsResults = this.props.results.toJS();
         var items = jsResults[this.props.recipe -1].map(x => {
             return <ListItem disabled={true}
                 primaryText={<a href={x.href}>{x.title}</a>}
-                secondaryText={<p><span>{x.ingredients.length}</span>{x.ingredients}</p>}/>
+                secondaryText={<p><span>{x.ingredients.length}</span>{this.listIngredients(x.ingredients.split(','))}</p>}/>
             });
         return(<span>{items}</span>);
     }
 }
+
